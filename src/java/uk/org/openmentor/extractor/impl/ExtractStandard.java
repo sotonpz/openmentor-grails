c39b943ef7704d360a8d040626c976cf5a23a19c
sotonpz <pz@ecs.soton.ac.uk>
2012-06-14 13:24:17 +0100

    Added field stripping logic to the standard extractor

--- src/java/uk/org/openmentor/extractor/impl/ExtractStandard.java
+++ src/java/uk/org/openmentor/extractor/impl/ExtractStandard.java
@@ -1,26 +1,3 @@
-/* =======================================================================
- * Copyright 2004-2012 The OpenMentor Team.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ========================================================================
- */
-
-
-/*
- * Rewritten to use POI scratchpad directly. This works well for .doc files, but not
- * for .docx. A different extractor is required for that. 
- */
-
 package uk.org.openmentor.extractor.impl;
 
 import uk.org.openmentor.extractor.Extractor;
@@ -33,6 +10,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.regex.Pattern;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -83,9 +61,44 @@ public class ExtractStandard implements Extractor {
     	inputStream.close();
     	
     	String[] commentsArray = extractor.getCommentsText();
+    	List<String> commentList = Arrays.asList(commentsArray);
+
     	comments.clear();
-    	comments.addAll(Arrays.asList(commentsArray));
+    	for(String comment : commentList) {
+    		comments.add(stripFields(comment));
+    	}    	
     	body = extractor.getText();
+    }
+    
+    /*
+     * Annotations in a few cases may contain fields. Actually, so may all the
+     * text, which is irritating. So we need a way of skipping out all the
+     * field stuff, and for this, regular expressions are a good solution.
+     * Essentially, we should look for ASCII 19, which starts a field, and
+     * ASCII 21, which ends the field. Delete these, and all other text, except
+     * stuff which follows ASCII 20, which is the text view.
+     */
+    private static Pattern fieldPattern = Pattern.compile("\\x13[^\\x14\\x15]*(?:\\x14([^\\x15]*))?\\x15");
+    
+    private static Pattern cleanPattern = Pattern.compile("\\x0b");
+    
+    /*
+     * TODO Make this handle nested fields, which can occur according to POI documentation 
+     */
+    private static String stripFields(String text) {
+        /*
+         * Amended to remove all field information - even the visible text. This
+         * is a bad idea in one sense - strictly it is invalid, as we are not
+         * really correct about annotation boundaries. But in another sense,
+         * fields are not written by people, so we can probably ignore them
+         * validly anyway.
+         */
+        String result = fieldPattern.matcher(text).replaceAll("");
+        result = cleanPattern.matcher(result).replaceAll("");
+        if (log.isTraceEnabled()) {
+            log.trace("Removed field: " + result);
+        }
+        return result;
     }
 
     public String getBody() {
@@ -96,4 +109,3 @@ public class ExtractStandard implements Extractor {
     	return comments;
     }
 }
-
